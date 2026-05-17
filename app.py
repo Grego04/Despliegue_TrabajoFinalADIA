aa8b9c013eac946371f601e308f9352e63a90470
Grego04 <133240525+Grego04@users.noreply.github.com>
2026-05-16 20:25:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ try:
         st.info("Asegúrate de haber ejecutado `drive.mount('/content/drive')` en tu celda de Colab antes de lanzar Streamlit.")
         st.stop()
         
-    scaler = joblib.load('scaler.pkl'))
+    scaler = joblib.load('scaler.pkl')
     model = joblib.load('modelo_burnout.pkl')
     
 except Exception as e:
